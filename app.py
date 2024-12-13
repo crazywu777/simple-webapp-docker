610d7f6a296bb2ddf67017cafca0593950c119ce
crazywu777 <123620594+crazywu777@users.noreply.github.com>
2024-12-13 18:06:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Welcome V2!"
+    return "Welcome V3!"
 
 @app.route('/how are you')
 def hello():
-    return 'I am good, how about you? V2'
+    return 'I am good, how about you? V3'
 
 if __name__ == "__main__":
     app.run()
